f46a1e39f7faa8a11c95d134fb7da0baf4b36f20
Jawad Ahmed <34104787+jawad-ahmed23@users.noreply.github.com>
2023-11-12 02:21:58 +0500

    update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,5 @@ export const EMAIL_REGEX =
   // eslint-disable-next-line no-useless-escape
   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-const LIVE_KEY = "6e7fa3ee2940ac9c0a307b28ff7eaff8";
-
-export const ODDS_API_KEY = process.env.ODDS_API_KEY || LIVE_KEY;
+export const ODDS_API_KEY =
+  process.env.ODDS_API_KEY || "76873652c65271f478f9fc077e2fe144";
