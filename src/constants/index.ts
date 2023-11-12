76fc2b3538e471ec60b43f2f7fa3e9065c5136f1
Jawad Ahmed <34104787+jawad-ahmed23@users.noreply.github.com>
2023-11-12 13:03:30 +0500

    add env var

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,8 @@ export const EMAIL_REGEX =
   // eslint-disable-next-line no-useless-escape
   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
+console.log(import.meta.env);
+
 console.log("process.env", import.meta.env.ODDS_API_KEY);
 
 export const ODDS_API_KEY =
