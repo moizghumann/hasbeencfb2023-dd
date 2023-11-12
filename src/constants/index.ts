6a157988889b7be43f2b019a710080644817140c
Jawad Ahmed <34104787+jawad-ahmed23@users.noreply.github.com>
2023-11-12 16:13:19 +0500

    remove vars console

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,8 +2,6 @@ export const EMAIL_REGEX =
   // eslint-disable-next-line no-useless-escape
   /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-console.log(import.meta.env);
-
 export const ODDS_API_KEY =
   import.meta.env.VITE_VERCEL_ODDS_API_KEY ||
   "45710ede53833fd76d543b75da1bc759";
